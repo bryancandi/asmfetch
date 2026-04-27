27c3fa34cac4b8c1edcd5a973c493fdd95c91fc9
Bryan C <7554866+bryancandi@users.noreply.github.com>
2026-04-27 15:01:29 -0700

    Format build.bat for improved readability

--- build.bat
+++ build.bat
@@ -2,5 +2,12 @@
 
 if not exist build mkdir build
 
-ml64.exe /c /I inc\ /Fo build\ src\main.asm src\cpu.asm src\memory.asm src\os.asm src\uptime.asm src\utility.asm
+ml64.exe /c /I inc\ /Fo build\ ^
+    src\main.asm ^
+    src\cpu.asm ^
+    src\memory.asm ^
+    src\os.asm ^
+    src\uptime.asm ^
+    src\utility.asm
+
 link.exe /OUT:build\asmfetch.exe build\*.obj /SUBSYSTEM:console /ENTRY:main
