7734abefb1e1dac85085589a4f9f13408dd60e9e
Bryan C <7554866+bryancandi@users.noreply.github.com>
2026-05-18 18:51:32 -0700

    Refactor asmfetch utility: move legacy modular version to separate branch

--- build.bat
+++ build.bat
@@ -1,13 +1,4 @@
 @echo off
 
-if not exist build mkdir build
-
-ml64.exe /c /I inc\ /Fo build\ ^
-    src\main.asm ^
-    src\cpu.asm ^
-    src\memory.asm ^
-    src\os.asm ^
-    src\uptime.asm ^
-    src\utility.asm
-
-link.exe /OUT:asmfetch.exe build\*.obj /SUBSYSTEM:console /ENTRY:main
+ml64.exe /c asmfetch.asm
+link.exe asmfetch.obj /SUBSYSTEM:console /ENTRY:main /OUT:asmfetch.exe
