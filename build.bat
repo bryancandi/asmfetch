27f47a282f7ff3e94d322050b3dc79ecedd592bc
Bryan C <7554866+bryancandi@users.noreply.github.com>
2026-08-03 07:24:19 -0700

    Add resource file and update build script

--- build.bat
+++ build.bat
@@ -1,4 +1,10 @@
 @echo off
 
 ml64.exe /c asmfetch.asm
-link.exe asmfetch.obj /SUBSYSTEM:console /ENTRY:start /OUT:asmfetch.exe
+if errorlevel 1 goto :eof
+
+rc.exe asmfetch.rc
+if errorlevel 1 goto :eof
+
+link.exe asmfetch.obj asmfetch.res /SUBSYSTEM:console /ENTRY:start /OUT:asmfetch.exe
+if errorlevel 1 goto :eof
